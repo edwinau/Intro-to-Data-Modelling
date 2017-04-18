a4ace525d7a446170c01ffb11fac3cffc0879d72
EDWIN AU <edwinau@rogers.com>
2017-04-18 14:25:40 -0400

    Many-to-Many Solution 1

---
+++ ManytoMany/db/migrate/20170418182137_create_article_users.rb
@@ -0,0 +1,8 @@
+class CreateArticleUsers < ActiveRecord::Migration[5.0]
+  def change
+    create_table :article_users do |t|
+      t.integer :article_id
+      t.integer :user_id
+    end
+  end
+end
